9f41b6542ae309ab45152622327377c2e2c54db8
PelleRemus <remusnicolaepelle@gmail.com>
2023-10-18 23:12:57 +0300

    sol level 3

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,47 @@
 import { Level, RunTypes } from "../lib/caroshark";
-import { winnings, fight } from "./utils";
+import { fight, tournamentAfterTwoRounds } from "./utils";
 
 const runType = (process.argv[2] as RunTypes) || "example";
 
-const level = new Level({ level: 2 });
+const level = new Level({ level: 3 });
 
 level.main = async (data: Array<any>, subLevel: number) => {
   const [n, m] = (data[0] as string).split(" ").map((x) => +x);
   let result = [];
   for (let i = 1; i <= n; i++) {
-    let tournament = data[i];
-
-    for (let k=0; k<2; k++) {
-      let nextBracket = "";
-      for (let j = 0; j < tournament.length; j += 2) {
-        let left: "P" | "R" | "S" = tournament[j];
-        let right = tournament[j + 1];
-        nextBracket += fight(left, right);
-      }
-      tournament = nextBracket;
+    let [rock, paper, scissors] = data[i].split(' ').map((x: string) => {
+        return {
+            type: x.slice(-1),
+            amount: +x.slice(0, -1)
+        }
+    });
+    let tournament = "";
+    while(rock.amount > 0) {
+        if(paper.amount == 1) {
+            tournament += "RP";
+            rock.amount--;
+            paper.amount--;
+            if(rock.amount) {
+                const rockAmount = Math.min(rock.amount, 3)
+                tournament += "R".repeat(rockAmount);
+                rock.amount -= rockAmount;
+            }
+        } else {
+            const rockAmount = Math.min(rock.amount, 3)
+            tournament += "R".repeat(rockAmount);
+            tournament += "P";
+            rock.amount -= rockAmount;
+            paper.amount--;
+        }
     }
-
+    if(paper.amount < 0)
+        console.log(paper.amount)
+    tournament += "P".repeat(paper.amount);
+    tournament += "S".repeat(scissors.amount);
+    
     result.push(tournament);
   }
-  return result.join("\n");
+  return result.join('\n');
 };
 
 level.generateOutput({
--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,19 @@ export const fight = (left: "P" | "R" | "S", right: string) => {
   return left == right ? left : winnings[left] == right ? left : right;
 };
 
+export const tournamentAfterTwoRounds = (tournament: string): string => {
+    for (let k=0; k<2; k++) {
+        let nextBracket = "";
+        for (let j = 0; j < tournament.length; j += 2) {
+          let left = tournament[j];
+          let right = tournament[j + 1];
+          nextBracket += fight(left as any, right);
+        }
+        tournament = nextBracket;
+    }
+    return tournament;
+}
+
 // export class Tournament {
 //     m: number;
 // }

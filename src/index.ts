1634e4cf6d89a4625cfbf76d8898116841c6a735
Domotor Zsolti <d.zsolti@yahoo.com>
2023-10-20 16:07:11 +0300

    level 1 done

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import { caroshark, LevelData, runType } from "./setup";
 caroshark.main = async (data: LevelData) => {
 
     // code here 
+    let result = []
+    for(let [x,y] of data.coordonates)
+        result.push(data.matrix[y][x])
 
-    return data;
+    return result.join('\n');
 };
 
 caroshark.generateOutput({

952861aaa7d6e6231fa156e61537ae243ad1d61b
PelleRemus <remusnicolaepelle@gmail.com>
2023-10-20 17:32:29 +0300

    dfs update

--- src/utils.ts
+++ src/utils.ts
@@ -38,20 +38,19 @@ export function findIslands(matrix) {
     return islands;
 }
 
-export const dfs = (matrix, m, coordinate, visited, destination) => {
+export const dfs = (matrix, m, coordinate, visited, path) => {
     if(coordinate.x < 0 || coordinate.x >= m || coordinate.y < 0 || coordinate.y >= m
-        || matrix[coordinate.y][coordinate.x] == 'W' || visited[coordinate.y][coordinate.x]) {
+        || matrix[coordinate.y][coordinate.x] == 0) {
+            path.push(coordinate);
             return;
     }
-    visited[coordinate.y][coordinate.x] = true;
-    if(coordinate.x == destination.x && coordinate.y == destination.y) {
-        return true;
+    if(visited[coordinate.y][coordinate.x]) {
+        return;
     }
+    visited[coordinate.y][coordinate.x] = true;
 
-    dfs(matrix, m, { x: coordinate.x - 1, y: coordinate.y }, visited, destination); // Up
-    dfs(matrix, m, { x: coordinate.x + 1, y: coordinate.y }, visited, destination); // Down
-    dfs(matrix, m, { x: coordinate.x, y: coordinate.y - 1 }, visited, destination); // Left
-    dfs(matrix, m, { x: coordinate.x, y: coordinate.y + 1 }, visited, destination); // Right
-
-    return false;
+    dfs(matrix, m, { x: coordinate.x - 1, y: coordinate.y }, visited, path); // Up
+    dfs(matrix, m, { x: coordinate.x + 1, y: coordinate.y }, visited, path); // Down
+    dfs(matrix, m, { x: coordinate.x, y: coordinate.y - 1 }, visited, path); // Left
+    dfs(matrix, m, { x: coordinate.x, y: coordinate.y + 1 }, visited, path); // Right
 }
